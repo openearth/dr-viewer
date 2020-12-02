5e3dbbfc668340c3cce8e8face39845c11033530
CindyvdVries <cindyvdvries@gmail.com>
2020-12-02 11:37:00 +0100

    layers

--- src/components/app-sidebar.vue
+++ src/components/app-sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer clipped app permanent width="400">
-    <router-view v-on="$listeners" /> 
+  <v-navigation-drawer clipped app permanent width="550">
+    <router-view v-on="$listeners" />
   </v-navigation-drawer>
 </template>
